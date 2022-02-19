86c9968bccfb7531b6a9c92e05d516177da36eba
1exz <95166102+1exz@users.noreply.github.com>
2022-02-19 18:44:32 +0300

    Update variable.js

--- variable.js
+++ variable.js
@@ -6,5 +6,5 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
 */
 function positiveSum(arr) {
-  return arr.filter(elem=>elem>0).reduce((a,b)=>a+b)
+  return arr.filter(elem=>elem>0).reduce((a,b)=>a+b,0)
 }
