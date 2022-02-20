c283d51ee2330eab5ee324b4ddc03b86aecdbb45
1exz <alex.buchko.m@gmail.com>
2022-02-20 10:15:06 +0300

    [Multiple of indexм]

--- variable.js
+++ variable.js
@@ -1,10 +1,17 @@
 /*
-It's the academic year's end, fateful moment of your school report. The averages must be calculated. 
-All the students come to you and entreat you to calculate their average for them.
- Easy ! You just need to write a script.
-Return the average of the given array rounded down to its nearest integer.
-The array will never be empty.
+Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+Some cases:
+
+[22, -6, 32, 82, 9, 25] => [-6, 32, 25]
+
+[68, -1, 1, -7, 10, 10] => [-1, 10]
+
+[-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+ 
 */
-function getAverage (marks) {
-  return Math.floor(marks.reduce((a, b) => a + b, 0) / marks.length);
-}
+  function multipleOfIndex(array){
+    return array.filter((element, index) => element % index === 0)
+           
+    }
+
