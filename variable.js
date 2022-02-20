75af710473963a79fe7a75dd9a03ad6fff79d28d
1exz <alex.buchko.m@gmail.com>
2022-02-20 19:39:04 +0300

    [Remove String Spaces]

--- variable.js
+++ variable.js
@@ -1,17 +1,8 @@
 /*
-Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
-
-Some cases:
-
-[22, -6, 32, 82, 9, 25] => [-6, 32, 25]
-
-[68, -1, 1, -7, 10, 10] => [-1, 10]
-
-[-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+Simple, remove the spaces from the string, then return the resultant string.
  
 */
-  function multipleOfIndex(array){
-    return array.filter((element, index) => element % index === 0)
-           
-    }
-
+function noSpace(x){
+return x.split(" ").join("")
+}
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
