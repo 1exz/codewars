12a567e834d2d36e0d8b05650b47fd1e080b4359
1exz <alex.buchko.m@gmail.com>
2022-03-14 22:56:24 +0300

    Merge two sorted arrays into one

--- variable.js
+++ variable.js
@@ -1,7 +1,10 @@
-/*
-Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+/*You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one,
+ sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. 
+If both arr1 and arr2 are empty, then just return an empty array.
+Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. 
+Remove duplicated in the returned result.
 */
-function grow(x){
-   return x.reduce((a,b)=>a*b,1)
-}
-console.log(grow([4, 1, 1, 1, 4]))
+function mergeArrays(arr1,arr2) {
+      return   Array.from(new Set(arr1.concat(arr2).sort((a,b)=>a-b))) 
+    }
